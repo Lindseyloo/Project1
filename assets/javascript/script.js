133b42d082c1dcb620b59951831f6671a7f21505
Zach Timmerman <zachtimmerman@Zachs-MacBook-Pro.local>
2018-12-26 11:10:36 -0700

    data displays on table in groups of top three results

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,10 +1,16 @@
 // Initialize Firebase
-//var config = {
-//api info will go here
-//};
-//firebase.initializeApp(config);
+var config = {
+  apiKey: "AIzaSyDcte2uBaoWV9plgoh8459pOgT-7ciQ-Bk",
+  authDomain: "project-1-3dbd1.firebaseapp.com",
+  databaseURL: "https://project-1-3dbd1.firebaseio.com",
+  projectId: "project-1-3dbd1",
+  storageBucket: "",
+  messagingSenderId: "778205183350"
+};
+firebase.initializeApp(config);
 
-//var database = firebase.database();
+var database = firebase.database();
+
 var userLocationInfo = {
   zip: "84123",
   latitude: "",
@@ -21,16 +27,16 @@ var bestBuySearch = "";
 var bestBuyItemSKU = ""
 var amazonItemResult = "";
 var lowestPrice = "";
-var resultItems = 
-  {
+var resultItems =
+{
   item: "",
-  store:"",
+  store: "",
   price: "",
-  };
+};
 
-  
-  
-  
+
+
+
 //Best Buy Item URL builder  
 var bestBuyitemURL = {
 
@@ -56,20 +62,20 @@ var bestBuyInStoreURL = {
 
 
 
-  /*Takes the last items searched and creates "&search=" for every empty character in the users item search
-  * returns the @param bestBuySearch item that is used to build the best buy URL
-  */
- function bestBuyKeywordConfig(){
- 
+/*Takes the last items searched and creates "&search=" for every empty character in the users item search
+* returns the @param bestBuySearch item that is used to build the best buy URL
+*/
+function bestBuyKeywordConfig() {
+
   //console.log(itemArray.length);
   arrayLength = itemArray.length - 1;
   var item = itemArray[arrayLength];
   console.log(item);
   bestBuySearch = "";
-  for(i = 0; i < item.length; i++){
+  for (i = 0; i < item.length; i++) {
     var itemChar = item.charAt(i);
     console.log(itemChar);
-    if(itemChar === " "){
+    if (itemChar === " ") {
       bestBuySearch += "&search=";
       console.log(bestBuySearch);
     }
@@ -80,133 +86,188 @@ var bestBuyInStoreURL = {
   }
   console.log(bestBuySearch);
   console.log(bestBuyitemURL.keyword);
- };
+};
 
- 
 
-  //Find Best Buy Items in stock based on customers location
-  function BBlocalUserItem(){
-    queryURL =
+
+//Find Best Buy Items in stock based on customers location
+function BBlocalUserItem() {
+  queryURL =
     bestBuyInStoreURL.baseURL +
     bestBuyInStoreURL.areaFunction +
     bestBuyInStoreURL.inStoreAvailability + bestBuyItemSKU + "))" +
     bestBuyInStoreURL.apiKey +
     bestBuyInStoreURL.show +
     bestBuyInStoreURL.responseFormat
-    console.log(queryURL);
-  
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-      .then(function(response) {
-        console.log(response);
-        var results = response.stores[0].products;
-        console.log(response.stores[0].products[0].name)
-        console.log(results);
-         
-        
-        for(i = 0; i < results.length; i++){
+  console.log(queryURL);
 
-         let resultItems =  {
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    .then(function (response) {
+      console.log(response);
+      var results = response.stores[0].products;
+      console.log(response.stores[0].products[0].name)
+      console.log(results);
+
+
+      for (i = 0; i < results.length; i++) {
+
+        let resultItems = {
           item: results[i].name,
-          store:"Best Buy",
+          store: "Best Buy",
           price: results[i].salePrice,
-          };
-      
-        bestbuyItemResult.push(resultItems);  
-        console.log("script.js-114",bestbuyItemResult);
         };
-        console.log(resultItems);
-        console.log(bestbuyItemResult);
+
+        bestbuyItemResult.push(resultItems);
+        console.log("script.js-114", bestbuyItemResult);
+      };
+
+      var bestBuyItem = bestbuyItemResult[0].item;
+      var bestBuyStore = bestbuyItemResult[0].store;
+      var bestBuyPrice = bestbuyItemResult[0].price;
+      var bestBuyItemSecond = bestbuyItemResult[1].item;
+      var bestBuyStoreSecond = bestbuyItemResult[1].store;
+      var bestBuyPriceSecond = bestbuyItemResult[1].price;
+      var bestBuyItemThird = bestbuyItemResult[2].item;
+      var bestBuyStoreThird = bestbuyItemResult[2].store;
+      var bestBuyPriceThird = bestbuyItemResult[2].price;
+   
+      // Pushing Best Buy Results to table.
+      //Push result to firebase database.
+      database.ref().push({
+        bestBuyItem: bestBuyItem,
+        bestBuyStore: bestBuyStore,
+        bestBuyPrice: bestBuyPrice,
+        bestBuyItemSecond: bestBuyItemSecond,
+        bestBuyStoreSecond: bestBuyStoreSecond,
+        bestBuyPriceSecond: bestBuyPriceSecond,
+        bestBuyItemThird: bestBuyItemThird,
+        bestBuyStoreThird: bestBuyStoreThird,
+        bestBuyPriceThird: bestBuyPriceThird,
+     
+      });
+      console.log(resultItems);
+      console.log(bestbuyItemResult);
     });
-    console.log(bestBuyItemSKU)
-    };
+  console.log(bestBuyItemSKU)
+};
+database.ref().on("child_added", function (childSnapshot) {
+
+  console.log(childSnapshot.val().bestBuyItem);
+  console.log(childSnapshot.val().bestBuyStore);
+  console.log(childSnapshot.val().bestBuyPrice);
+  console.log(childSnapshot.val().bestBuyItemSecond);
+  console.log(childSnapshot.val().bestBuyStoreSecond);
+  console.log(childSnapshot.val().bestBuyPriceSecond);
+  console.log(childSnapshot.val().bestBuyItemThird);
+  console.log(childSnapshot.val().bestBuyStoreThird);
+  console.log(childSnapshot.val().bestBuyPriceThird);
+
+
+
+  // displays data to table body
+  bestBuyItem = childSnapshot.val().bestBuyItem;
+  bestBuyStore = childSnapshot.val().bestBuyStore;
+  bestBuyPrice = childSnapshot.val().bestBuyPrice;
+  bestBuyItemSecond = childSnapshot.val().bestBuyItemSecond;
+  bestBuyStoreSecond = childSnapshot.val().bestBuyStoreSecond;
+  bestBuyPriceSecond = childSnapshot.val().bestBuyPriceSecond;
+  bestBuyItemThird = childSnapshot.val().bestBuyItemThird;
+  bestBuyStoreThird = childSnapshot.val().bestBuyStoreThird;
+  bestBuyPriceThird = childSnapshot.val().bestBuyPriceThird;
+
+
+$("#tableBody").append("<tr><td>" + bestBuyItem + "</td><td>" + bestBuyStore + "</td><td>" + bestBuyPrice + "</td></tr>" + "<tr><td>" + bestBuyItemSecond + "</td><td>"  + bestBuyStoreSecond + "</td><td>" + bestBuyPriceSecond + "</td></tr>" + "<tr><td>" + bestBuyItemThird + "</td><td>"  + bestBuyStoreThird + "</td><td>" + bestBuyPriceThird + "</td><tr>")})
 
 
 //onclick function that captures the users results and passes them through them
 // through bestBuyKeywordcfig
 
- $(".btn-submit").on("click", function(event){
+$(".btn-submit").on("click", function (event) {
   event.preventDefault();
 
   //collects user search item and stores them in an item array
   var item = $("#item").val().trim();
+  $("#item").val('');
+
   console.log(item);
   itemArray.push(item);
   console.log(itemArray);
-   bestBuyKeywordConfig();
+  bestBuyKeywordConfig();
 
-   var queryURL = 
+  var queryURL =
     bestBuyitemURL.baseURL +
     bestBuyitemURL.keyword + bestBuySearch + ")" +
-    bestBuyitemURL.attribute + 
+    bestBuyitemURL.attribute +
     bestBuyitemURL.apiKey +
     bestBuyitemURL.showOptions +
     bestBuyitemURL.facets +
     bestBuyitemURL.responseFormat;
   console.log(queryURL);
-    // Performing our AJAX GET request
-    
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-     .then(function(response) {
-    
-        var results = response.products;
-        bestBuyItemSKU = "";
-        //console.log(results.length);
-        for(i = 0; i < results.length; i++ ){
-          bestBuyItemSKU += results[i].sku + ",";
-          //resultItems.item
-        }
-       
-        console.log(results[0]);
-        console.log(bestBuyItemSKU);
-        BBlocalUserItem();
+  // Performing our AJAX GET request
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    .then(function (response) {
+
+      var results = response.products;
+      bestBuyItemSKU = "";
+      //console.log(results.length);
+      for (i = 0; i < results.length; i++) {
+        bestBuyItemSKU += results[i].sku + ",";
+        //resultItems.item
+      }
+
+      console.log(results[0]);
+      console.log(bestBuyItemSKU);
+      BBlocalUserItem();
     });
 
-    //Waits until the SKU numbers get pulled from the Users Search
-    //setTimeout(function(){ BBlocalUserItem();}, 1000);
+  //Waits until the SKU numbers get pulled from the Users Search
+  //setTimeout(function(){ BBlocalUserItem();}, 1000);
 
- });
- console.log(bestBuyItemSKU)
- 
- 
+});
+console.log(bestBuyItemSKU)
 
- $("#zip-code-btn").on("click", function(event){
+
+
+$("#zip-code-btn").on("click", function (event) {
   event.preventDefault();
 
   var zip = $("#zip-code").val().trim();
   userLocationInfo.zip = zip;
   console.log(zip);
   var queryURL =
-  "https://maps.googleapis.com/maps/api/geocode/json?address=" +
-  userLocationInfo.zip +
-  "&key=" +
-  googleAPIkey;
+    "https://maps.googleapis.com/maps/api/geocode/json?address=" +
+    userLocationInfo.zip +
+    "&key=" +
+    googleAPIkey;
 
   $.ajax({
     url: queryURL,
     method: "GET"
   })
     // After the data comes back from the API
-    .then(function(response) {
+    .then(function (response) {
       console.log(response);
       // Storing an array of results in the results variable
       userLocationInfo.latitude = response.results[0].geometry.location.lat;
       console.log(userLocationInfo.latitude);
       userLocationInfo.longitude = response.results[0].geometry.location.lng;
       console.log(userLocationInfo.longitude);
-  });
- });
-/*If user ignores the zipcode input pop up modal to remind them to add zipcode. Function called if adding information in the 
+    });
+});
 
 
-*/
 
-  
+
+
+
+
 // // Get the modal
 // var modal = document.getElementById('myModal');
 
@@ -230,5 +291,5 @@ var bestBuyInStoreURL = {
 // window.onclick = function(event) {
 //   if (event.target == modal) {
 //     modal.style.display = "none";
-  // }
-// }
+
+  //
